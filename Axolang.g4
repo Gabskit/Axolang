eb0bf151f1e64e80f13cd98dc93f44bf53cdb34b
Gabskit <gaby.mofa31@gmail.com>
2026-07-05 21:20:09 -0600

    hi

--- Axolang.g4
+++ Axolang.g4
@@ -2,7 +2,7 @@ grammar Axolang;
 
 // Regla raíz del programa
 program
-: ' '*? statement* EOF
+: statement* EOF
 ;
 
 // Sentencias válidas en Axolang
@@ -13,12 +13,11 @@ statement
 
 // Declaración de variables (Tipos primitivos, Auto-inferencia y Sintaxis de Array Exacta)
 varDeclaration
-: type ' ' IDENTIFIER '=' expression
-| type ' ' IDENTIFIER '[' INT_LITERAL? ']' '=' arrayLiteral
-| type ' ' IDENTIFIER
-| type ' ' IDENTIFIER '[' INT_LITERAL ']'
+: type IDENTIFIER '=' expression
+| type IDENTIFIER '[' INT_LITERAL? ']' '=' arrayLiteral
+| type IDENTIFIER
+| type IDENTIFIER '[' INT_LITERAL ']'
 ;
-
 type
 : 'xxsvar' | 'xsvar' | 'svar' | 'var' | 'lvar' | 'xlvar' | 'xxlvar' | 'fun'
 ;
@@ -34,14 +33,14 @@ matchCase
 ;
 
 declaration
-:  IDENTIFIER '=' expression
-|  IDENTIFIER '(' OTHER ')'
+: IDENTIFIER '=' expression
+| IDENTIFIER '(' OTHER ')'
 | statement
 ;
 
 // Un paquete contiene declaraciones internas estructuradas
 pkg
-: ' '*? '{' varDeclaration* '}'
+: '{' varDeclaration* '}'
 ;
 
 // Expresiones jerárquicas de Axolang
@@ -61,24 +60,23 @@ expression
 ;
 
 arrayLiteral
-: '«' ' '? expression (',' ' '? expression)* '»'
+: '«' expression (',' expression)* '»'
 ;
 
 arrayAccess
 : IDENTIFIER '[' expression ']'
 ;
 
-// --- TOKENS LÉXICOS ---
-IDENTIFIER : [\u{1F4A9}\u{1F926}\p{Alpha}\p{General_Category=Other_Letter}][\u{1F4A9}\u{1F926}\p{Alnum}\p{General_Category=Other_Letter}]* ;
-CHAR_LITERAL : [ \t]*? '\'' [\u{1F4A9}\u{1F926}\p{Alnum}\p{General_Category=Other_Letter}] '\'';
-STRING_LITERAL : [ \t]*? '"' [ \t\n\u{1F4A9}\u{1F926}\p{Alnum}\p{General_Category=Other_Letter}]* '"';
-INT_LITERAL : [ \t]*? [+-]? [ \t]*? [0-9]+ ;
-UINT_LITERAL : [ \t]*? [0-9]+ 'u';
-FLOAT_LITERAL : [ \t]*? [+-]? [ \t]*? [0-9]+ ('.' [0-9]+)?;
-DECIMAL_LITERAL : [ \t]*? [+-]? [ \t]*? [0-9]+ ('.' [0-9]+)? [dD];
-COMPLEX_LITERAL : [ \t]*? [+-]? [ \t]*? [0-9]+ ('.' [0-9]+)? [ \t]*? [+-] [ \t]*? [0-9]+ ('.' [0-9]+)? [ \t]*? 'i'
-| [ \t]*? [+-]? [ \t]*? [0-9]+ ('.' [0-9]+)? [ \t]*? 'i';
-BOOLEAN : [ \t]*? 'true' | [ \t]*? 'false';
+// --- TOKENS LÉXICOS LIMPIOS (Sin capturar espacios previos) ---
+IDENTIFIER : [a-zA-Z_\u{1F4A9}\u{1F926}\p{Alpha}\p{General_Category=Other_Letter}] [a-zA-Z0-9_\u{1F4A9}\u{1F926}\p{Alnum}\p{General_Category=Other_Letter}]* ;
+CHAR_LITERAL : '\'' [\u{1F4A9}\u{1F926}\p{Alnum}\p{General_Category=Other_Letter}] '\'';
+STRING_LITERAL : '"' [ \t\n\u{1F4A9}\u{1F926}\p{Alnum}\p{General_Category=Other_Letter}]* '"';
+INT_LITERAL : [+-]? [0-9]+ ;
+UINT_LITERAL : [0-9]+ 'u';
+FLOAT_LITERAL : [+-]? [0-9]+ ('.' [0-9]+)?;
+DECIMAL_LITERAL : [+-]? [0-9]+ ('.' [0-9]+)? [dD];
+COMPLEX_LITERAL : [+-]? [0-9]+ ('.' [0-9]+)? [ \t]*? [+-] [ \t]*? [0-9]+ ('.' [0-9]+)? [ \t]*? 'i'
+| [+-]? [0-9]+ ('.' [0-9]+)? [ \t]*? 'i';
+BOOLEAN : 'true' | 'false';
 WS : [ \t\r\n]+ -> skip ;
-
 OTHER : .+? ;
--- transpilador.js
+++ transpilador.js
@@ -19,300 +19,6 @@ const HEADER_C = `
 #else
 #define AXO_INIT_CONSOLE() (void)0
 #endif
-
-// --- FUNCIONES HELPER DE IMPRESIÓN (Para evitar Type Aliasing en _Generic) ---
-static inline void _axo_print_char8(char8_t c) {
-  printf("%c\t", (char)c);
-}
-
-static inline void _axo_print_char16(char16_t c) {
-  printf("%lc\t", (wint_t)c);
-}
-
-static inline void _axo_print_char32(char32_t c) {
-  char buf[5] = {
-    0
-  };
-  mbstate_t st = {
-    0
-  };
-  c32rtomb(buf, c, &st);
-  printf("%s\t", buf);
-}
-
-static inline void _axo_print_float16_complex(_Float16 complex c) {
-  printf("%f + %fi\t", crealf(c), cimagf(c));
-}
-
-static inline void _axo_print_float_complex(float complex c) {
-  printf("%f + %fi\t", crealf(c), cimagf(c));
-}
-
-static inline void _axo_print_double_complex(double complex c) {
-  printf("%lf + %lfi\t", creal(c), cimag(c));
-}
-
-static inline void _axo_print_ldouble_complex(long double complex c) {
-  printf("%Lf + %Lfi\t", creall(c), cimagl(c));
-}
-
-// Helpers para enteros gigantes de lvar y xlvar (Conversión segura a texto para printf)
-static inline void _axo_print_int128(_BitInt(128) n) {
-  if (n == 0) {
-    printf("0\t"); return;
-  }
-  if (n < 0) {
-    printf("-"); n = -n;
-  }
-  char buf[40]; int i = 39; buf[i--] = '\0';
-  while (n > 0) {
-    buf[i--] = '0' + (int)(n % 10); n /= 10;
-  }
-  printf("%s\t", &buf[i + 1]);
-}
-
-static inline void _axo_print_uint128(unsigned _BitInt(128) n) {
-  if (n == 0) {
-    printf("0\t"); return;
-  }
-  char buf[40]; int i = 39; buf[i--] = '\0';
-  while (n > 0) {
-    buf[i--] = '0' + (int)(n % 10); n /= 10;
-  }
-  printf("%s\t", &buf[i + 1]);
-}
-
-static inline void _axo_print_int256(_BitInt(256) n) {
-  if (n == 0) {
-    printf("0\t"); return;
-  }
-  if (n < 0) {
-    printf("-"); n = -n;
-  }
-  char buf[80]; int i = 79; buf[i--] = '\0';
-  while (n > 0) {
-    buf[i--] = '0' + (int)(n % 10); n /= 10;
-  }
-  printf("%s\t", &buf[i + 1]);
-}
-
-static inline void _axo_print_uint256(unsigned _BitInt(256) n) {
-  if (n == 0) {
-    printf("0\t"); return;
-  }
-  char buf[80]; int i = 79; buf[i--] = '\0';
-  while (n > 0) {
-    buf[i--] = '0' + (int)(n % 10); n /= 10;
-  }
-  printf("%s\t", &buf[i + 1]);
-}
-
-// --- MACRO SOBRECARGADO SEGURO ESTÁTICO DE C23 ---
-#define axo_print_single(X) _Generic((X), \
-bool:                      printf((X) ? "true\t" : "false\t"), \
-int8_t:                    printf("%" PRId8 "\t", (int8_t)(X)), \
-uint8_t:                   printf("%" PRIu8 "\t", (uint8_t)(X)), \
-int16_t:                   printf("%" PRId16 "\t", (int16_t)(X)), \
-uint16_t:                  printf("%" PRIu16 "\t", (uint16_t)(X)), \
-int32_t:                   printf("%" PRId32 "\t", (int32_t)(X)), \
-uint32_t:                  printf("%" PRIu32 "\t", (uint32_t)(X)), \
-int64_t:                   printf("%" PRId64 "\t", (int64_t)(X)), \
-uint64_t:                  printf("%" PRIu64 "\t", (uint64_t)(X)), \
-_BitInt(128):              _axo_print_int128(X), \
-unsigned _BitInt(128):     _axo_print_uint128(X), \
-_BitInt(256):              _axo_print_int256(X), \
-unsigned _BitInt(256):     _axo_print_uint256(X), \
-_Float16:                  printf("%f\t", (float)(X)), \
-float:                     printf("%f\t", (float)(X)), \
-double:                    printf("%lf\t", (double)(X)), \
-long double:               printf("%Lf\t", (long double)(X)), \
-_Float256:                 printf("%Lf\t", (long double)(X)), \
-char8_t:                   _axo_print_char8(X), \
-char16_t:                  _axo_print_char16(X), \
-char32_t:                  _axo_print_char32(X), \
-char*:                     printf("%s\t", (char*)(X)), \
-const char*:               printf("%s\t", (const char*)(X)), \
-_Float16 complex: _axo_print_float16_complex(X), \
-float complex:             _axo_print_float_complex(X), \
-double complex:            _axo_print_double_complex(X), \
-long double complex:       _axo_print_ldouble_complex(X), \
-_Decimal32:                printf("%Hf\t", (_Decimal32)(X)), \
-_Decimal64:                printf("%Df\t", (_Decimal64)(X)), \
-_Decimal128:               printf("%DDf\t", (_Decimal128)(X)), \
-default:                   printf("[Objeto/Bloque void*]\t") \
-)
-// === COPIAR Y PEGAR DENTRO DE HEADER_C EN TU TRANSPILADOR ===
-
-// 1. FUNCIONES AUXILIARES SEGURAS DE ESCANEO (Soporte Unicode, Complejos y 256-bits)
-static inline void _axo_scan_string_safe(char* dest, size_t max_len) {
-  char formato[32];
-  // Construye de forma segura el límite de caracteres (ej: "%127s")
-  snprintf(formato, sizeof(formato), "%%%zds", max_len - 1);
-  if (scanf(formato, dest) != 1) {
-    // Manejo silencioso o asignación vacía si falla
-    dest[0] = '\0';
-  }
-}
-
-// Helpers para caracteres Unicode nativos por niveles
-static inline void _axo_scan_char8(char8_t* ptr) {
-  char c;
-  if (scanf(" %c", &c) == 1) *ptr = (char8_t)c;
-}
-
-static inline void _axo_scan_char16(char16_t* ptr) {
-  wchar_t wc;
-  if (scanf(" %lc", &wc) == 1) *ptr = (char16_t)wc;
-}
-
-static inline void _axo_scan_char32(char32_t* ptr) {
-  wchar_t wc;
-  if (scanf(" %lc", &wc) == 1) *ptr = (char32_t)wc;
-}
-
-// Helper seguro para evitar el desbordamiento de Stack de _Float16
-static inline void _axo_scan_float16(_Float16* ptr) {
-  float temp;
-  if (scanf("%f", &temp) == 1) *ptr = (_Float16)temp;
-}
-
-// Helpers para números complejos nativos
-static inline void _axo_scan_xscom(_Float16 complex* ptr) {
-  float r,
-  i;
-  if (scanf("%f %f", &r, &i) == 2) *ptr = (_Float16)r + (_Float16)i * I;
-}
-
-static inline void _axo_scan_scom(float complex* ptr) {
-  float r,
-  i;
-  if (scanf("%f %f", &r, &i) == 2) *ptr = r + i * I;
-}
-
-static inline void _axo_scan_com(double complex* ptr) {
-  double r,
-  i;
-  if (scanf("%lf %lf", &r, &i) == 2) *ptr = r + i * I;
-}
-
-static inline void _axo_scan_lcom(long double complex* ptr) {
-  long double r,
-  i;
-  if (scanf("%Lf %Lf", &r, &i) == 2) *ptr = r + i * I;
-}
-
-// --- ALGORITMOS DE PARSEO PARA TIPOS MÁXIMOS DE 128 Y 256 BITS ---
-static inline void _axo_scan_int128(_BitInt(128)* ptr) {
-  char buf[45];
-  if (scanf("%44s", buf) == 1) {
-    _BitInt(128) res = 0;
-    int i = 0,
-    sign = 1;
-    if (buf[0] == '-') {
-      sign = -1; i++;
-    }
-    else if (buf[0] == '+') {
-      i++;
-    }
-    while (buf[i] >= '0' && buf[i] <= '9') {
-      res = res * 10 + (buf[i] - '0');
-      i++;
-    }
-    *ptr = res * sign;
-  }
-}
-
-static inline void _axo_scan_uint128(unsigned _BitInt(128)* ptr) {
-  char buf[45];
-  if (scanf("%44s", buf) == 1) {
-    unsigned _BitInt(128) res = 0;
-    int i = 0;
-    if (buf[0] == '+') {
-      i++;
-    }
-    while (buf[i] >= '0' && buf[i] <= '9') {
-      res = res * 10 + (buf[i] - '0');
-      i++;
-    }
-    *ptr = res;
-  }
-}
-
-static inline void _axo_scan_int256(_BitInt(256)* ptr) {
-  char buf[85];
-  if (scanf("%84s", buf) == 1) {
-    _BitInt(256) res = 0;
-    int i = 0,
-    sign = 1;
-    if (buf[0] == '-') {
-      sign = -1; i++;
-    }
-    else if (buf[0] == '+') {
-      i++;
-    }
-    while (buf[i] >= '0' && buf[i] <= '9') {
-      res = res * 10 + (buf[i] - '0');
-      i++;
-    }
-    *ptr = res * sign;
-  }
-}
-
-static inline void _axo_scan_uint256(unsigned _BitInt(256)* ptr) {
-  char buf[85];
-  if (scanf("%84s", buf) == 1) {
-    unsigned _BitInt(256) res = 0;
-    int i = 0;
-    if (buf[0] == '+') {
-      i++;
-    }
-    while (buf[i] >= '0' && buf[i] <= '9') {
-      res = res * 10 + (buf[i] - '0');
-      i++;
-    }
-    *ptr = res;
-  }
-}
-
-static inline void _axo_scan_float256(_Float256* ptr) {
-  long double temp;
-  if (scanf("%Lf", &temp) == 1) *ptr = (_Float256)temp;
-}
-
-
-// 2. MACRO EVALUADORA DE PUNTEROS PARA SCAN (Totalmente Blindada en C23)
-#define axo_scan_single(X) _Generic((X), \
-bool*:                  _Generic((X), bool*: ({ int tmp; int r = scanf("%d", &tmp); if(r==1)*(X)=(bool)tmp; r; })), \
-int8_t*:                scanf("%" SCNd8, (int8_t*)(X)), \
-uint8_t*:               scanf("%" SCNu8, (uint8_t*)(X)), \
-int16_t*:               scanf("%" SCNd16, (int16_t*)(X)), \
-uint16_t*:              scanf("%" SCNu16, (uint16_t*)(X)), \
-int32_t*:               scanf("%" SCNd32, (int32_t*)(X)), \
-uint32_t*:              scanf("%" SCNu32, (uint32_t*)(X)), \
-int64_t*:               scanf("%" SCNd64, (int64_t*)(X)), \
-uint64_t*:              scanf("%" SCNu64, (uint64_t*)(X)), \
-_BitInt(128)*:          _axo_scan_int128((_BitInt(128)*)(X)), \
-unsigned _BitInt(128)*: _axo_scan_uint128((unsigned _BitInt(128)*)(X)), \
-_BitInt(256)*:          _axo_scan_int256((_BitInt(256)*)(X)), \
-unsigned _BitInt(256)*: _axo_scan_uint256((unsigned _BitInt(256)*)(X)), \
-_Float16*:              _axo_scan_float16((_Float16*)(X)), \
-float*:                 scanf("%f", (float*)(X)), \
-double*:                scanf("%lf", (double*)(X)), \
-long double*:           scanf("%Lf", (long double*)(X)), \
-_Float256*:             _axo_scan_float256((_Float256*)(X)), \
-char8_t*:               _axo_scan_char8((char8_t*)(X)), \
-char16_t*:              _axo_scan_char16((char16_t*)(X)), \
-char32_t*:              _axo_scan_char32((char32_t*)(X)), \
-char*:                  _axo_scan_string_safe((char*)(X), 128), \
-_Float16 complex*:      _axo_scan_xscom((_Float16 complex*)(X)), \
-float complex*:         _axo_scan_scom((float complex*)(X)), \
-double complex*:        _axo_scan_com((double complex*)(X)), \
-long double complex*:   _axo_scan_lcom((long double complex*)(X)), \
-_Decimal32*:            scanf("%Hf", (_Decimal32*)(X)), \
-_Decimal64*:            scanf("%Df", (_Decimal64*)(X)), \
-_Decimal128*:           scanf("%DDf", (_Decimal128*)(X)), \
-default:                printf("[Error: Tipo de puntero no escaneable]\n") \
-)
 typedef struct {
   int64_t value;
   bool has_error;
@@ -390,6 +96,16 @@ class AxolangToCListener extends AxolangListener {
 		this.tablaSimbolos = new Map();
 	}
 	enterVarDeclaration(ctx) {
+		let cp = ctx.parentCtx;
+		while (cp) {
+			if (
+				cp.constructor.name === "PkgContext" ||
+				cp.constructor.name === "Pkg"
+			) {
+				return;
+			}
+			cp = cp.parentCtx;
+		}
 		const id = ctx.IDENTIFIER().getText();
 		let type = ctx.getChild(0).getText();
 
@@ -432,15 +148,33 @@ class AxolangToCListener extends AxolangListener {
 					// Retorna el sub-array formateado como Compound Literal estático en C23
 					return `{ .axo_other = (${type}[${subExpressions.length}]){ ${subInicializadores.join(", ")} } }`;
 				} else if (nodoExpression.pkg()) {
-					let pkgvars = [];
-					pkgvars.push(`{ .axo_other = ( struct {`);
+					// 1. Obtenemos la lista de variables del paquete
 					const pkgbody = nodoExpression.pkg().varDeclaration();
+					let structMembers = [];
+					let structInitializers = [];
+
 					for (let p = 0; p < pkgbody.length; p++) {
-						let pkgvalue = pkgbody[p].getText();
-						pkgvars.push(`${pkgvalue};`);
+						const decl = pkgbody[p];
+
+						// Extraemos el tipo (primer hijo, ej: "xxsvar") y el identificador (ej: "a")
+						const pType = decl.getChild(0).getText();
+						const pId = decl.IDENTIFIER().getText();
+
+						// Creamos la definición limpia para el cuerpo del struct en C: "xxsvar a;"
+						structMembers.push(`${pType} ${pId};`);
+
+						// Si la variable del paquete tiene un valor asignado (ej: = 5)
+						if (decl.expression()) {
+							const pExpr = decl.expression();
+							// LLAMADA RECURSIVA: transpilamos el valor asignado con el formato correcto (.axo_int, etc.)
+							const pValTranspiled = transformarExpresion(pExpr);
+							structInitializers.push(`.${pId} = ${pValTranspiled}`);
+						}
 					}
-					pkgvars.push(`}){} }`);
-					return `${pkgvars.join(" ")}`;
+
+					// 2. Retornamos un Compound Literal de C23 perfectamente formateado
+					// Nota que usamos un puntero '&' para que quepa en el campo '.axo_other' de la unión
+					return `{ .axo_other = &(struct { ${structMembers.join(" ")} }){ ${structInitializers.join(", ")} } }`;
 				} else if (value.includes(".")) {
 					value += chkmrk[0];
 					return `{ .axo_flt = ${value} }`;
@@ -487,26 +221,134 @@ class AxolangToCListener extends AxolangListener {
 			inititem.push(`};`);
 			this.outputC += `    ${type} ${id}[${elementsCount}] = { ${inititem.join(" ")}\n`;
 		} else {
-			let valvar = ctx.expression().getText();
-			this.outputC += ` ${type} ${id};`;
-			switch (type) {
-				case "xxsvar":
-					if (valvar.startsWith("'")) {
-						this.outputC += `\n ${id}.axo_char = ${valvar};`;
-					} else if (
-						(valvar.toLowerCase().includes("true") ||
-							valvar.toLowerCase().includes("false")) &&
-						!valvar.startsWith("'")
-					) {
-						this.outputC += `\n ${id}.axo_boo = ${valvar};`;
-					} else if (valvar.endsWith("u")){
-					  valvar = valvar.replace(/u$/, "")
-					  this.outputC += `\n ${id}.axo_intu = ${valvar};`
-					}
-					break;
-				default:
-				// code
-			}
+			this.outputC += `\n ${type} ${id};`;
+			const globalvar = ctx => {
+			if (ctx.expression()) {
+				let valvar = ctx.expression().getText();
+
+				switch (type) {
+					case "xxsvar":
+						if (valvar.startsWith("'")) {
+							this.outputC += `\n ${id}.axo_chara = ${valvar};`;
+						} else if (
+							(valvar.toLowerCase().includes("true") ||
+								valvar.toLowerCase().includes("false")) &&
+							!valvar.startsWith('"')
+						) {
+							this.outputC += `\n ${id}.axo_boo = ${valvar};`;
+						} else if (valvar.endsWith("u")) {
+							valvar = valvar.replace(/u$/, "");
+							this.outputC += `\n ${id}.axo_intu = ${valvar};`;
+						} else if (ctx.expression().IDENTIFIER()) {
+							this.outputC += `\n ${id} = ${valvar};`;
+						} else {
+							this.outputC += `\n ${id}.axo_int = ${valvar};`;
+						}
+						break;
+					case "xsvar":
+						if (valvar.includes(".")) {
+							this.outputC += `\n ${id}.axo_flt = ${valvar}f16;`;
+						} else if (valvar.startsWith("'")) {
+							this.outputC += `\n ${id}.axo_chara = u${valvar};`;
+						} else if (
+							(valvar.toLowerCase().includes("true") ||
+								valvar.toLowerCase().includes("false")) &&
+							!valvar.startsWith('"')
+						) {
+							this.outputC += `\n ${id}.axo_boo = ${valvar};`;
+						} else if (valvar.endsWith("u")) {
+							valvar = valvar.replace(/u$/, "");
+							this.outputC += `\n ${id}.axo_intu = ${valvar};`;
+						} else if (ctx.expression().IDENTIFIER()) {
+							this.outputC += `\n ${id} = ${valvar};`;
+						} else {
+							this.outputC += `\n ${id}.axo_int = ${valvar};`;
+						}
+						break;
+					case "svar":
+						if (valvar.includes(".")) {
+							this.outputC += `\n ${id}.axo_flt = ${valvar}f;`;
+						} else if (valvar.endsWith("D")) {
+							valvar = valvar.replace(/D$/, "DF");
+							this.outputC += `\n ${id}.axo_dec = ${valvar};`;
+						} else if (valvar.endsWith("i")) {
+							valvar = valvar.replace(/i$/, "f16i");
+							this.outputC += `\n ${id}.axo_com = ${valvar};`;
+						} else if (valvar.startsWith("'")) {
+							this.outputC += `\n ${id}.axo_chara = U${valvar};`;
+						} else if (
+							(valvar.toLowerCase().includes("true") ||
+								valvar.toLowerCase().includes("false")) &&
+							!valvar.startsWith('"')
+						) {
+							this.outputC += `\n ${id}.axo_boo = ${valvar};`;
+						} else if (valvar.endsWith("u")) {
+							valvar = valvar.replace(/u$/, "");
+							this.outputC += `\n ${id}.axo_intu = ${valvar};`;
+						} else if (ctx.expression().IDENTIFIER()) {
+							this.outputC += `\n ${id} = ${valvar};`;
+						} else {
+							this.outputC += `\n ${id}.axo_int = ${valvar};`;
+						}
+						break;
+					case "var":
+						if (valvar.includes(".")) {
+							this.outputC += `\n ${id}.axo_flt = ${valvar};`;
+						} else if (valvar.endsWith("D")) {
+							valvar = valvar.replace(/D$/, "DD");
+							this.outputC += `\n ${id}.axo_dec = ${valvar};`;
+						} else if (valvar.endsWith("i")) {
+							valvar = valvar.replace(/i$/, "* I");
+							this.outputC += `\n ${id}.axo_com = ${valvar};`;
+						} else if (valvar.startsWith("'")) {
+							this.outputC += `\n ${id}.axo_chara = U${valvar};`;
+						} else if (valvar.startsWith('"')) {
+							this.outputC += `\n ${id}.axo_other = U${valvar};`;
+						} else if (
+							(valvar.toLowerCase().includes("true") ||
+								valvar.toLowerCase().includes("false")) &&
+							!valvar.startsWith('"')
+						) {
+							this.outputC += `\n ${id}.axo_boo = ${valvar};`;
+						} else if (ctx.expression().pkg()) {
+							// 1. Obtenemos la lista de variables del paquete
+							const pkgbody = ctx.expression().pkg().varDeclaration();
+							let structMembers = [];
+							let structInitializers = [];
+
+							for (let p = 0; p < pkgbody.length; p++) {
+								const decl = pkgbody[p];
+
+								// Extraemos el tipo (primer hijo, ej: "xxsvar") y el identificador (ej: "a")
+								const pType = decl.getChild(0).getText();
+								const pId = decl.IDENTIFIER().getText();
+
+								// Creamos la definición limpia para el cuerpo del struct en C: "xxsvar a;"
+								structMembers.push(`${pType} ${pId};`);
+
+								// Si la variable del paquete tiene un valor asignado (ej: = 5)
+								if (decl.expression()) {
+									const pExpr = decl.expression();
+									// LLAMADA RECURSIVA: transpilamos el valor asignado con el formato correcto (.axo_int, etc.)
+									structInitializers.push(`.${pId} = .${pvaltype} =`);
+								}
+							}
+
+							// 2. Retornamos un Compound Literal de C23 perfectamente formateado
+							// Nota que usamos un puntero '&' para que quepa en el campo '.axo_other' de la unión
+							this.outputC += `\n  ${id}.axo_other = &(struct { ${structMembers.join(" ")} }){ ${structInitializers.join(", ")} } `;
+						} else if (valvar.endsWith("u")) {
+							valvar = valvar.replace(/u$/, "");
+							this.outputC += `\n ${id}.axo_intu = ${valvar};`;
+						} else if (ctx.expression().IDENTIFIER()) {
+							this.outputC += `\n ${id} = ${valvar};`;
+						} else {
+							this.outputC += `\n ${id}.axo_int = ${valvar};`;
+						}
+						break;
+					default:
+				}
+			}}
 		}
 	}
 
@@ -524,6 +366,7 @@ function transpile(inputCode) {
 	const tokens = new antlr4.CommonTokenStream(lexer);
 	const parser = new AxolangParser(tokens);
 	const tree = parser.program();
+	//console.log(tree);
 
 	const listener = new AxolangToCListener();
 	antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
@@ -531,9 +374,16 @@ function transpile(inputCode) {
 	let finalCode = HEADER_C + "\nint main() {\n";
 	finalCode += listener.outputC;
 	finalCode += "    return 0;\n}";
-
 	return finalCode;
 }
-const test = `xxsvar hi = true
+const test = `
+var hi[] = «5, 6u, «7.5, {
+  var uno = 6
+  var dod = 9i
+}»»
+var go = {
+  var algo = "Hola desde pkg"
+}
+var text = go
 `;
 console.log(transpile(test));
