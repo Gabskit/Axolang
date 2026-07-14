ae47725e2d36acfce0a72c4927a88e239d9c5beb
Gabskit <gaby.mofa31@gmail.com>
2026-07-13 20:04:59 -0600

    header

--- Src/transpilador.js
+++ Src/transpilador.js
@@ -12,13 +12,12 @@ const HEADER_C = `
 #include <wchar.h>
 #include <inttypes.h>
 
-// Parche de compatibilidad Unicode para consolas Windows
-#ifdef _WIN32
-#include <windows.h>
-#define AXO_INIT_CONSOLE() SetConsoleOutputCP(CP_UTF8)
-#else
-#define AXO_INIT_CONSOLE() (void)0
-#endif
+#define dec_complex(type) _dec_complex_##type
+
+// Estructuras de arrays contiguos bajo el capó
+typedef _Decimal32  _dec_complex__Decimal32[2];
+typedef _Decimal64  _dec_complex__Decimal64[2];
+
 typedef struct {
   int64_t value;
   bool has_error;
@@ -47,7 +46,7 @@ typedef union {
   bool axo_boo;
   int64_t axo_int;
   uint64_t axo_intu;
-  float complex axo_com;
+  dec_complex(_Decimal32) complex axo_com;
   _Decimal64 axo_dec;
   void* axo_other;
 } var;
@@ -57,7 +56,7 @@ typedef union {
   bool axo_boo;
   _BitInt(128) axo_int;
   unsigned _BitInt(128) axo_intu;
-  double complex axo_com;
+  dec_complex(_Decimal64) complex axo_com;
   _Decimal128 axo_dec;
   void* axo_other;
 } lvar;
@@ -66,26 +65,24 @@ typedef union {
 class AxolangToCListener extends AxolangListener {
   constructor() {
     super();
-    this.outputC = "";
+    this.varAndFun = "";
+    this.scriptLines = "";
     this.isComplexMatch = false;
     this.matchExpr = "";
     this.caseCount = 0;
-    this.tablaSimbolos = new Map();
-  }
+    this.tablaSimbolos = new Map();}
+
   enterVarDeclaration(ctx) {
     const id = ctx.IDENTIFIER().getText();
     let type = ctx.getChild(0).getText();
-
     let cp = ctx.parentCtx;
     while (cp) {
       if (
         cp.constructor.name === "PkgContext" ||
-        cp.constructor.name === "Pkg"
-      ) {
-        return;
-      }
-      cp = cp.parentCtx;
-    }
+        cp.constructor.name === "Pkg") {
+        return;}
+      cp = cp.parentCtx;}
+
     let chkmrk = ["DD", "U", "* I"];
     if (type.startsWith("xs")) {
       chkmrk = ["f16", "u", ""];
@@ -94,9 +91,9 @@ class AxolangToCListener extends AxolangListener {
     } else if (type.startsWith("l")) {
       chkmrk = ["DL", "U", "* I"];
     } else {
-      chkmrk = ["DD", "U", "* I"];
-    }
-if (type == "pkg"){
+      chkmrk = ["DD", "U", "* I"];}
+
+    if (type == "pkg"){
   let pkgvars = ctx.expression().pkg().varDeclaration()
   let pvdec = []
   for(let p = 0; p < pkgvars.length;p++){
@@ -104,86 +101,71 @@ if (type == "pkg"){
     let pvtp = pkgvars[p].getChild(0).getText()
     pvdec.push(`${pvtp} ${pvid};`)
   }
-  this.outputC += `\ntypedef struct{ ${pvdec.join("\n")} }${id};`
-}
-    else if (ctx.arrayLiteral()) {
+      this.varAndFun += `\ntypedef struct{\n ${pvdec.join("\n")}}${id};`}
+      else if (ctx.arrayLiteral()) {
       const values = ctx.arrayLiteral().expression();
       const numval = values.length;
       let vars = [];
-
       for (let a = 0; a < numval; a++) {
         let thisval = values[a].getText();
-        if (thisval.endsWith("u")) {
+        if (thisval.endsWith("u") && ((!thisval.startsWith("'") || !thisval.startsWith('"')) || !values[a].IDENTIFIER())) {
           thisval = thisval.replace(/u$/, "");
           vars.push(`{ .axo_intu = ${thisval} }`);
-        } else if (thisval.endsWith("i")) {
+        } else if (thisval.endsWith("i") && ((!thisval.startsWith("'") || !thisval.startsWith('"')) || !values[a].IDENTIFIER())) {
           thisval = thisval.replace(/i$/, chkmrk[2]);
-
           vars.push(`{ .axo_com = ${thisval} }`);
-        } else if (thisval.includes(".")) {
+        } else if (thisval.includes(".") && ((!thisval.startsWith("'") || !thisval.startsWith('"')) || !values[a].IDENTIFIER())) {
           thisval += chkmrk[0];
           vars.push(`{ .axo_dec= ${thisval} }`);
         } else if (
           (thisval.includes("true") || thisval.includes("false")) &&
           (!(thisval.includes('"') || thisval.includes("'")) ||
-            values[a].IDENTIFIER())
-        ) {
+            values[a].IDENTIFIER())) {
           vars.push(`{ .axo_boo = ${thisval} }`);
         } else if (thisval.startsWith("'")) {
           vars.push(`{ .axo_chara= ${chkmrk[1]}${thisval} }`);
         } else if (thisval.startsWith('"')) {
           vars.push(`{ .axo_other = ${chkmrk[1]}${thisval} }`);
-        } else if (thisval.startsWith("&")) {
+        } else if (thisval.startsWith("&") && ((!thisval.startsWith("'") || !thisval.startsWith('"')) || !values[a].IDENTIFIER())) {
           vars.push(`{ .axo_other = ${thisval} }`);
         } else if (values[a].IDENTIFIER()) {
           vars.push(`{ .axo_other = ${thisval} }`);
-        } else if (values[a].arrayLiteral()) {
-          
-          this.enterVarDeclaration(values[a]);
-          vars.push(`{ .axo_other = ${thisval} }`);
-        } else {
-          vars.push(`{ .axo_int = ${thisval} }`);
-        }
-      }
-      this.outputC += `\n${type} ${id}[${numval}] = { ${vars.join(",")} };`;
+        } /*else if (values[a].arrayLiteral()) {
+          al(values[a]);
+        //  vars.push(`{ .axo_other = ${thisval} }`);
+        }*/ else {
+          vars.push(`{ .axo_int = ${thisval} }`);}}
+      this.varAndFun += `\n${type} ${id}[${numval}] = { ${vars.join(",")} };`;
     } else {
-      this.outputC += `\n${type} ${id};`;
+      this.varAndFun += `\n${type} ${id};`;
       let thisvar = ctx.expression();
       let value = thisvar.getText();
-      if (value.endsWith("u")) {
+      if (value.endsWith("u") && ((!value.startsWith("'") || !value.startsWith('"')) || !thisvar.IDENTIFIER())) {
         value = value.replace(/u$/, "");
-        this.outputC += `\n${id}.axo_intu = ${value};`;
-      } else if (value.endsWith("i")) {
+        this.varAndFun += `\n${id}.axo_intu = ${value};`;
+      } else if (value.endsWith("i") && ((!value.startsWith("'") || !value.startsWith('"')) || !thisvar.IDENTIFIER())) {
         value = value.replace(/i$/, chkmrk[2]);
-
-        this.outputC += `\n${id}.axo_com = ${value};`;
-      } else if (value.includes(".")) {
+        this.varAndFun += `\n${id}.axo_com = ${value};`;
+      } else if (value.includes(".") && ((!value.startsWith("'") || !value.startsWith('"')) || !thisvar.IDENTIFIER())) {
         value += chkmrk[0];
-        this.outputC += `\n${id}.axo_dec = ${value};`;
+        this.varAndFun += `\n${id}.axo_dec = ${value};`;
       } else if (
         (value.includes("true") || value.includes("false")) &&
-        (!(value.includes('"') || value.includes("'")) || thisvar.IDENTIFIER())
-      ) {
-        this.outputC += `\n${id}.axo_boo = ${value};`;
+        (!(value.includes('"') || value.includes("'")) || thisvar.IDENTIFIER())) {
+        this.varAndFun += `\n${id}.axo_boo = ${value};`;
       } else if (value.startsWith("'")) {
-        this.outputC += `\n${id}.axo_chara= ${chkmrk[1]}${value};`;
+        this.varAndFun += `\n${id}.axo_chara= ${chkmrk[1]}${value};`;
       } else if (value.startsWith('"')) {
-        this.outputC += `\n${id}.axo_other = ${chkmrk[1]}${value}";`;
-      } else if (value.startsWith("&")) {
-        this.outputC += `\n${id}.axo_other = ${value};`;
+        this.varAndFun += `\n${id}.axo_other = ${chkmrk[1]}${value}";`;
+      } else if (value.startsWith("&") && ((!value.startsWith("'") || !value.startsWith('"')) || !thisvar.IDENTIFIER())) {
+        this.varAndFun += `\n${id}.axo_other = ${value};`;
       } else if (thisvar.IDENTIFIER()) {
-        this.outputC += `\n${id} = ${value};`;
-      }
-    }
-  }
+        this.varAndFun += `\n${id} = ${value};`;}}}
 
   exitMatchStatement(ctx) {
     if (!this.isComplexMatch) {
-      this.outputC += `    }\n`;
-    }
-    this.isComplexMatch = false;
-  }
-}
+      this.varAndFun += `    }\n`;}
+    this.isComplexMatch = false;}}
 
 function transpile(inputCode) {
   const chars = new antlr4.InputStream(inputCode);
@@ -192,22 +174,24 @@ function transpile(inputCode) {
   const parser = new AxolangParser(tokens);
   const tree = parser.program();
   //console.log(tree);
-
   const listener = new AxolangToCListener();
   antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
-
   let finalCode = HEADER_C;
-  finalCode += listener.outputC;
-  return finalCode;
-}
+  finalCode += listener.varAndFun;
+  finalCode += `\nint main(int argc, char* argv[]){`
+  finalCode += listener.scriptLines;
+  finalCode += `}`
+  return finalCode;}
+
 const test = `
-var hi[] = «5, 6u, 5.5-6i, «7, 7.8»»
+var hi[] = «5, 'i', 5.5-6i, "hi"»
 var go = "Hola"
 var text = go
 lvar hi2 = 7 - 8i
-xsvar oh = 8.7
+xsvar oh = 'i'
 pkg hola = {
   var numero
-}
+  xsvar id}
 `;
+
 console.log(transpile(test));
